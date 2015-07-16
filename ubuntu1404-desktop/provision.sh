79d14fb2eb705e84ed34c696f0205d1d5325eb6c
unknown <tboyce@tboyce.mmgins.local>
2015-07-16 08:23:22 -0400

    first commit

---
+++ ubuntu1404-desktop/provision.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Add NodeJS repository
+curl --silent --location https://deb.nodesource.com/setup_0.12 | bash -
+
+# Install packages
+#apt-get update
+#apt-get upgrade -y
+apt-get install -y vim nodejs git zsh rake vim-gtk openjdk-7-jre chromium-browser unity-chromium-extension
+npm install -g grunt-cli karma-cli yo generator-angular generator-angular2 generator-gulp-angular gulp bower
+
+# Download WebStorm
+if [ ! -f /vagrant/WebStorm-10.0.4.tar.gz ]; then
+  wget http://download.jetbrains.com/webstorm/WebStorm-10.0.4.tar.gz -P /vagrant/ --quiet
+fi
+
+# Configure Shell
+chsh -s /usr/bin/zsh vagrant
+
+# Configure User
+service lightdm stop
+su -c "source /vagrant/user-config.sh" vagrant
+service lightdm start
---
+++ ubuntu1404-desktop/user-config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install WebStorm
+tar xvfz /vagrant/WebStorm-10.0.4.tar.gz -C /home/vagrant
+mv /home/vagrant/WebStorm-141.1550 /home/vagrant/webstorm
+tar xvfz /vagrant/webstorm10-config.tar.gz -C /home/vagrant
+mkdir -p ~/.local/share/applications/
+cp /vagrant/jetbrains-webstorm.desktop ~/.local/share/applications/
+mkdir -p ~/.gnome/apps/
+cp /vagrant/jetbrains-webstorm.desktop ~/.gnome/apps/
+mkdir -p bin
+ln -sf /home/vagrant/webstorm/bin/webstorm.sh /home/vagrant/bin/webstorm
+source ~/.profile
+
+# Configure Launcher
+dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://chromium-browser.desktop', 'application://firefox.desktop', 'application://jetbrains-webstorm.desktop', 'application://gnome-terminal.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+
+# Configure Wallpaper
+dbus-launch --exit-with-session gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/Beach_by_Renato_Giordanelli.jpg
+
+# Configure ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i 's/robbyrussell/ys/g' .zshrc
+sed -i 's/plugins=(git)/plugins=(git npm bower)/' .zshrc
+
+# Configure git
+git config --global url."https://".insteadOf git://
